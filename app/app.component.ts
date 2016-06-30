48f623b56ea8dd8578b8e2a19f4459bff0a5b3a0
dance2die <dontbothermewithspam@gmail.com>
2016-06-30 06:36:36 -0400

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+import {ControlGroup, FormBuilder} from 'angular2/common';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <form [ngFormModel]="form">
+            <input type="text" ngControl="search">
+        </form>
+    `
+})
+export class AppComponent {
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder) {
+        this.form = fb.group({
+            search: []
+        });
+
+        var search = this.form.find('search');
+        search.valueChanges
+            .subscribe(x => console.log(x));
+    }
+}
